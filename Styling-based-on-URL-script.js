96429cc77c38e009cedf6835cd5cde228c06e0ce
DavidDelawa <38319473+DavidDelawa@users.noreply.github.com>
2022-06-28 09:10:06 +0100

    Add files via upload

---
+++ Styling-based-on-URL-script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    if (window.location.href.indexOf("cl=355") > -1) {
+        alert("Registration for our fringe events has now closed.");
+        document.getElementById("delegate-spc").style.display = "none";
+    }
+});
+
+
+$(document).ready(function() {
+    if (window.location.href.indexOf("cl=355") > -1) {
+        document.getElementById("#aoDynamicForm").style.display = "none";
+    }
+});
+
+$(document).ready(function() {
+    if (window.location.href.indexOf("cl=355") > -1) {
+        alert("Registration for our fringe events has now closed.");
+    }
+});
